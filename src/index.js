5ad14f3b802c84abd286fe706afa8610f9f004b2
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-08-05 15:38:49 +1000

    import firebase and keep the IDs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require("firebase");
+require("firebase/firestore");
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyDgyIL9Tyu6_I2VlSgNFg5VXjLIQd4916w",
+  authDomain: "chat-app-a3eb0.firebaseapp.com",
+  databaseURL: "https://chat-app-a3eb0.firebaseio.com",
+  projectId: "chat-app-a3eb0",
+  storageBucket: "chat-app-a3eb0.appspot.com",
+  messagingSenderId: "171623050926",
+  appId: "1:171623050926:web:3c23b2ef7878f283b5073f",
+  measurementId: "G-ZZWC1E6EKF"
+});
+
 ReactDOM.render(
   <React.StrictMode>
   <div>Hello, world!</div>
