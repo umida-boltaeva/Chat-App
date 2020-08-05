e799ebbb9dd3071e7ed37a19eb7b5a6dd25b0e65
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-08-05 16:10:00 +1000

    create components

---
+++ src/login/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class LoginComponent extends React.Component {
+    
+    render() {
+        return(<div>Hello World from Login</div>)
+    }
+}
+
+export default LoginComponent;
