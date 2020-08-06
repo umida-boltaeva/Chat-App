0f00197041779e168eb9370fcd584070fabc9338
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-08-06 18:42:26 +1000

    move all keys to dotenv

--- src/index.js
+++ src/index.js
@@ -7,19 +7,20 @@ import "bootstrap/dist/css/bootstrap.css";
 import LoginComponent from "./login/login";
 import SignupComponent from "./signup/signup";
 import DashboardComponent from "./dashboard/dashboard";
+require('dotenv').config();
 
 const firebase = require("firebase");
 require("firebase/firestore");
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDgyIL9Tyu6_I2VlSgNFg5VXjLIQd4916w",
-  authDomain: "chat-app-a3eb0.firebaseapp.com",
-  databaseURL: "https://chat-app-a3eb0.firebaseio.com",
-  projectId: "chat-app-a3eb0",
-  storageBucket: "chat-app-a3eb0.appspot.com",
-  messagingSenderId: "171623050926",
-  appId: "1:171623050926:web:3c23b2ef7878f283b5073f",
-  measurementId: "G-ZZWC1E6EKF"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 });
 
 const routing = (
