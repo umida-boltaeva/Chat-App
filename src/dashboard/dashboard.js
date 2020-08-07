97e6fd050f7a1b2b65f44a97f9ebea14d12e9ca7
Umida Boltaeva <54983810+umida-boltaeva@users.noreply.github.com>
2020-08-07 11:39:07 +1000

    Routing (#3)
    * install dependencies and libraries
    
    * install firebase and remove unnecessary files from react app
    
    * delete firebase scripts and keep <React.StrictMode>
    
    * import firebase and keep the IDs
    
    * create components
    
    * routing
    
    * add bootstrap and reactstrap
    
    * correct the typo
    
    * move all keys to dotenv

---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class DashboardComponent extends React.Component {
+    
+    render() {
+        return(<div>Hello World from Dashboard</div>)
+    }
+}
+
+export default DashboardComponent;
---
+++ src/signup/signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class SignupComponent extends React.Component {
+    
+    render() {
+        return(<div>Hello World from Signup</div>)
+    }
+}
+
+export default SignupComponent;
