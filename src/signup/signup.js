03911ba547d8f7b5007f65af92020695d8ee3085
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-08-07 19:09:06 +1000

    import material-ui items

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,10 +1,32 @@
 import React from "react";
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from "./styles"
+const firebase = require("firebase");
 
 class SignupComponent extends React.Component {
     
     render() {
-        return(<div>Hello World from Signup</div>)
+
+        const { classes } = this.props;
+
+        return(
+            <main className={classes.main}>
+                <CssBaseline></CssBaseline>
+                <Paper className={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                        Sign Up!
+                    </Typography>
+                </Paper>
+            </main>
+        )
     }
 }
 
-export default SignupComponent;
+export default withStyles(styles)(SignupComponent);
