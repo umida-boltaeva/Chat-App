85189b817b175ae85d58e157a54a0b4bb5d74d9f
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-08-09 19:22:37 +1000

    check the password confirmation

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -9,10 +9,19 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import styles from "./styles"
-import { Form } from "reactstrap";
 const firebase = require("firebase");
 
 class SignupComponent extends React.Component {
+
+    constructor () {
+        super();
+        this.state = {
+            email: null,
+            password: null,
+            passwordConfirmation: null,
+            signupError: ""
+        };
+    }
     
     render() {
 
@@ -29,7 +38,7 @@ class SignupComponent extends React.Component {
                         <FormControl required fullWidth margin="normal">
                             <InputLabel htmlFor="signup-email-input">Enter Your Email</InputLabel>
                             <Input autoComplete="email" 
-                                onChange={(e) => this.userTyping("type", e)} 
+                                onChange={(e) => this.userTyping("email", e)} 
                                 autoFocus 
                                 id="signup-email-input">
                             </Input> 
@@ -56,6 +65,13 @@ class SignupComponent extends React.Component {
                                 color="primary"
                                 className={classes.submit}>Submit</Button>
                     </form>
+                    {
+                        this.state.signupError ?
+                        <Typography className={classes.errorText} component="h5" variant="h6">
+                            {this.state.signupError}
+                        </Typography> : null
+                    }
+
                     <Typography component="h5" variant="h6" className={classes.hasAccountHeader}>Already Have An Account?</Typography>
                         <Link className={classes.logInLink} to="/login">Log In!</Link>
                 </Paper>
@@ -63,12 +79,30 @@ class SignupComponent extends React.Component {
         )
     };
 
+    formIsValid = () => this.state.password === this.state.passwordConfirmation;
+
     userTyping = (type, e) => {
-        console.log(type, e);
+        switch (type) {
+            case "email":
+                this.setState({ email: e.target.value });
+                break;
+            case "password":
+                this.setState({ password: e.target.value });
+                break;
+            case "passwordConfirmation":
+                this.setState({ passwordConfirmation: e.target.value });
+                break;
+        
+            default:
+                break;
+        }
     }
 
     submitSignup = e => {
-        console.log("submitting!");
+        e.preventDefault();
+        if(!this.formIsValid()) {
+            this.setState({signupError: "Passwords do not match!"})
+        }
     }
 }
 
