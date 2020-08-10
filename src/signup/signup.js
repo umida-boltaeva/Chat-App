5b702c7e494b928205f49d4c32d137eab72b313e
Umida Boltaeva <54983810+umida-boltaeva@users.noreply.github.com>
2020-08-10 22:36:13 +1000

    Signup form settings (#4)
    Build Signup form

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,10 +1,94 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import styles from "./styles";
+import { Form } from "reactstrap";
+const firebase = require("firebase");
 
 class SignupComponent extends React.Component {
-    
-    render() {
-        return(<div>Hello World from Signup</div>)
-    }
+  userTyping = (type, e) => {
+    console.log(type, e);
+  };
+
+  submitSignup = (e) => {
+    console.log("submitting!");
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.main}>
+        <CssBaseline></CssBaseline>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Sign Up!
+          </Typography>
+          <form onSubmit={(e) => this.submitSignup(e)} className={classes.form}>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="signup-email-input">
+                Enter Your Email
+              </InputLabel>
+              <Input
+                autoComplete="email"
+                onChange={(e) => this.userTyping("type", e)}
+                autoFocus
+                id="signup-email-input"
+              ></Input>
+            </FormControl>
+
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="signup-password-input">
+                Create A Password
+              </InputLabel>
+              <Input
+                type="password"
+                onChange={(e) => this.userTyping("password", e)}
+                id="signup-password-input"
+              ></Input>
+            </FormControl>
+
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="signup-password-confirmation-input">
+                Confirm Your Password
+              </InputLabel>
+              <Input
+                type="password"
+                onChange={(e) => this.userTyping("passwordConfirmation", e)}
+                id="signup-password-confirmation-input"
+              ></Input>
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Submit
+            </Button>
+          </form>
+          <Typography
+            component="h5"
+            variant="h6"
+            className={classes.hasAccountHeader}
+          >
+            Already Have An Account?
+          </Typography>
+          <Link className={classes.logInLink} to="/login">
+            Log In!
+          </Link>
+        </Paper>
+      </main>
+    );
+  }
 }
 
-export default SignupComponent;
+export default withStyles(styles)(SignupComponent);
