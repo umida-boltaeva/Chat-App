d70e950fd08dc9e0b1c2978fda9b50675b848edd
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-08-10 21:28:19 +1000

    seperate firebase connection logic.

--- src/index.js
+++ src/index.js
@@ -8,20 +8,6 @@ import LoginComponent from "./login/login";
 import SignupComponent from "./signup/signup";
 import DashboardComponent from "./dashboard/dashboard";
 
-const firebase = require("firebase");
-require("firebase/firestore");
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-});
-
 const routing = (
   <Router>
     <div id="routing-container">
