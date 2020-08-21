d21df3e2f791659fdcf5d4eecac9725452f93cd4
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-08-21 10:03:43 +1000

    create list item and avatar

--- src/chatlist/chatList.js
+++ src/chatlist/chatList.js
@@ -13,8 +13,65 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import NotificationImportant from "@material-ui/icons/NotificationImportant";
 
 class ChatListComponent extends React.Component {
+  newChat = () => {
+    console.log("New chat click");
+  };
+
+  selectChat = (index) => {
+    console.log("select chat", index);
+  };
   render() {
-    return <div>Hello</div>;
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.root}>
+        <Button
+          variant="conatined"
+          fullWidth
+          color="primary"
+          className={classes.newChatBtn}
+          onClick={this.newChat}
+        ></Button>
+        <List>
+          {this.props.chats.map((_chat, _index) => {
+            return (
+              <ListItem
+                onclick={() => this.selectChat(_index)}
+                className={classes.listItem}
+                selected={this.prop.selectedChatIndex === _index}
+                alignItems="flex-start"
+              >
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp">
+                    {
+                      _chat.users
+                        .filter((_user) => _user !== this.props.userEmail)[0]
+                        .split("")[0]
+                    }
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    _chat.users.filter(
+                      (_user) => _user !== this.props.userEmail
+                    )[0]
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography component="span" color="textPrimary">
+                        {_chat.messages[
+                          _chat.messages.length - 1
+                        ].message.subString(0, 30)}
+                      </Typography>
+                    </React.Fragment>
+                  }
+                ></ListItemText>
+              </ListItem>
+            );
+          })}
+        </List>
+      </main>
+    );
   }
 }
 
